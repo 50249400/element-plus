38d02ad73e1330e196d4ac59e9e700f7e72d2289
zhengwei <57430568+50249400@users.noreply.github.com>
2025-03-18 16:04:48 +0800

    Update popper-offset.vue

--- docs/examples/menu/popper-offset.vue
+++ docs/examples/menu/popper-offset.vue
@@ -7,6 +7,14 @@
     style="max-width: 600px"
   >
     <el-menu-item index="1">Processing Center</el-menu-item>
+    <el-menu-item index="1">Processing Center</el-menu-item>
+    <el-menu-item index="1">Processing Center</el-menu-item>
+    <el-menu-item index="1">Processing Center</el-menu-item>
+    <el-menu-item index="1">Processing Center</el-menu-item>
+    <el-menu-item index="1">Processing Center</el-menu-item>
+    <el-menu-item index="1">Processing Center</el-menu-item>
+    <el-menu-item index="1">Processing Center</el-menu-item>
+    <el-menu-item index="1">Processing Center</el-menu-item>
     <el-sub-menu index="2">
       <template #title>Workspace</template>
       <el-menu-item index="2-1">item one</el-menu-item>
